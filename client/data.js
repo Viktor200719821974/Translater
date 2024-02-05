1ac363584e476d995ebd573232615c0b89270682
Viktor200719821974 <vik200719821974@gmail.com>
2024-02-05 19:57:04 +0100

    first commit

---
+++ client/data.js
@@ -0,0 +1,342 @@
+export const data = [
+    {
+        "id": 1,
+        "english": "run out of",
+        "ukraine": "закінчується"
+    },
+    {
+        "id": 2,
+        "english": "get down to",
+        "ukraine": "приступимо"
+    },
+    {
+        "id": 3,
+        "english": "It's time to",
+        "ukraine": "пора"
+    },
+    {
+        "id": 4,
+        "english": "point out",
+        "ukraine": "помітити"
+    },
+    {
+        "id": 5,
+        "english": "go on",
+        "ukraine": "продовжувати"
+    },
+    {
+        "id": 6,
+        "english": "It goes without saying that",
+        "ukraine": "Само собою розуміється, що"
+    },{
+        "id": 7,
+        "english": "unless",
+        "ukraine": "покі ні"
+    },{
+        "id": 8,
+        "english": "going on",
+        "ukraine": "відбувається"
+    },{
+        "id": 9,
+        "english": "figure out",
+        "ukraine": "розуміти"
+    },{
+        "id": 10,
+        "english": "give up",
+        "ukraine": "сумувати"
+    },{
+        "id": 11,
+        "english": "whatever",
+        "ukraine": "щоб не (з яким)"
+    },{
+        "id": 12,
+        "english": "stick",
+        "ukraine": "притримуюсь"
+    },{
+        "id": 13,
+        "english": "I stick to the point that",
+        "ukraine": "Я притримуюсь тієї думки, що"
+    },{
+        "id": 14,
+        "english": "we face",
+        "ukraine": "ми зіткнулися"
+    },{
+        "id": 15,
+        "english": "sort out",
+        "ukraine": "владнати, вирішити"
+    },{
+        "id": 16,
+        "english": "to tell the truth",
+        "ukraine": "сказати правду"
+    },{
+        "id": 17,
+        "english": "if I were you",
+        "ukraine": "як би я був на твоєму місці "
+    },{
+        "id": 18,
+        "english": "as soon as posible",
+        "ukraine": "як можно скоріше"
+    },{
+        "id": 19,
+        "english": "put off",
+        "ukraine": "відкладати"
+    },{
+        "id": 20,
+        "english": "call off",
+        "ukraine": "відмінити"
+    },{
+        "id": 21,
+        "english": "do without",
+        "ukraine": "обійтись без"
+    },{
+        "id": 22,
+        "english": "It's no exeggeration to say",
+        "ukraine": "не буде перебільшенням сказати"
+    },{
+        "id": 23,
+        "english": "think over",
+        "ukraine": "обдумати"
+    },{
+        "id": 24,
+        "english": "hold on",
+        "ukraine": "почекати"
+    },{
+        "id": 25,
+        "english": "find out",
+        "ukraine": "виясняти"
+    },{
+        "id": 26,
+        "english": "the whole truth",
+        "ukraine": "всю правду"
+    },{
+        "id": 27,
+        "english": "get on with",
+        "ukraine": "ладнати"
+    },{
+        "id": 28,
+        "english": "put up with",
+        "ukraine": "миритися"
+    },{
+        "id": 29,
+        "english": "injustice",
+        "ukraine": "несправедливість"
+    },{
+        "id": 30,
+        "english": "any longer",
+        "ukraine": "довше"
+    },{
+        "id": 31,
+        "english": "look for",
+        "ukraine": "шукати"
+    },{
+        "id": 32,
+        "english": "further",
+        "ukraine": "подальшого"
+    },{
+        "id": 33,
+        "english": "throw away",
+        "ukraine": "викинути"
+    },{
+        "id": 34,
+        "english": "at once",
+        "ukraine": "відразу"
+    },{
+        "id": 35,
+        "english": "come in handy",
+        "ukraine": "знадобиться"
+    },{
+        "id": 36,
+        "english": "do over",
+        "ukraine": "переробити"
+    },{
+        "id": 37,
+        "english": "keep up with",
+        "ukraine": "поспіти, наздогнати"
+    },{
+        "id": 38,
+        "english": "catch up with",
+        "ukraine": "догнати"
+    },{
+        "id": 39,
+        "english": "a while",
+        "ukraine": "де-який час"
+    },{
+        "id": 40,
+        "english": "the rest",
+        "ukraine": "інші"
+    },{
+        "id": 41,
+        "english": "look after",
+        "ukraine": "доглядати"
+    },{
+        "id": 42,
+        "english": "fall behind",
+        "ukraine": "відстати"
+    },{
+        "id": 43,
+        "english": "calm down",
+        "ukraine": "заспокоїтися"
+    },{
+        "id": 44,
+        "english": "eat out",
+        "ukraine": "їсти поза домом"
+    },{
+        "id": 45,
+        "english": "what about",
+        "ukraine": "як нарахунок того"
+    },{
+        "id": 46,
+        "english": "how about",
+        "ukraine": "як нарахунок того"
+    },{
+        "id": 47,
+        "english": "get up",
+        "ukraine": "вставати"
+    },{
+        "id": 48,
+        "english": "used to",
+        "ukraine": "робив раніше"
+    },{
+        "id": 49,
+        "english": "wake up",
+        "ukraine": "прокидатися"
+    },{
+        "id": 50,
+        "english": "work out",
+        "ukraine": "займатися"
+    },{
+        "id": 51,
+        "english": "fill in",
+        "ukraine": "заповнювати"
+    },{
+        "id": 52,
+        "english": "turn on",
+        "ukraine": "увімкнути"
+    },{
+        "id": 53,
+        "english": "switch on",
+        "ukraine": "увімкнути"
+    },{
+        "id": 54,
+        "english": "turn off",
+        "ukraine": "вимкнути"
+    },{
+        "id": 55,
+        "english": "switch off",
+        "ukraine": "вимкнути"
+    },
+    {
+        "id": 56,
+        "english": "get in touch with",
+        "ukraine": "зв'язатися з"
+    },
+    {
+        "id": 57,
+        "english": "turn up",
+        "ukraine": "зробити голосніше"
+    },{
+        "id": 58,
+        "english": "turn down",
+        "ukraine": "зробити тихіше"
+    },{
+        "id": 59,
+        "english": "come back",
+        "ukraine": "повертатися"
+    },{
+        "id": 60,
+        "english": "get back",
+        "ukraine": "повертатися(до роботи, розмови)"
+    },{
+        "id": 61,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 62,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 63,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 64,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 65,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 66,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 67,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 68,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 69,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 70,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 71,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 72,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 73,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 74,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 75,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 76,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 77,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 78,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 79,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 80,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 81,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 82,
+        "english": "",
+        "ukraine": ""
+    },{
+        "id": 83,
+        "english": "",
+        "ukraine": ""
+    },
+];
